d3c85444a61fc31004d8c7dd71a9cd2b67d86297
neuecc <ils@neue.cc>
2020-07-20 19:37:59 +0900

    echo

--- sandbox/EchoServer/Program.cs
+++ sandbox/EchoServer/Program.cs
@@ -1,15 +1,38 @@
-﻿using Microsoft.Extensions.Hosting;
-using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EchoServer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // Console.WriteLine("Hello World!");
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureWebHost(builder =>
+                {
+                    builder
+                    .UseKestrel()
+                    .UseUrls("http://localhost:9999")
+                    .UseStartup<Startup>();
+                })
+                .RunConsoleAsync();
 
-            
+        }
+    }
+
+    class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            var hello = Encoding.UTF8.GetBytes("hello");
+
+            app.Run(async x =>
+            {
+                await x.Response.BodyWriter.WriteAsync(hello);
+            });
         }
     }
 }
