7ebe934437cc1d38c2651ba5e2090a56b00faa11
neuecc <ils@neue.cc>
2020-07-20 14:39:43 +0900

    Add EchoServer

---
+++ sandbox/EchoServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            
+        }
+    }
+}
