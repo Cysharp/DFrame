48a0b49324176acce8e513abdbc1836312ceb4d8
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-08-05 18:56:03 +0900

    feat: add Echo MagicOnion Server

---
+++ sandbox/EchoMagicOnion.Shared/EchoHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using MagicOnion;
+using MagicOnion.Server;
+using MagicOnion.Server.Hubs;
+using MessagePack;
+
+namespace EchoMagicOnion.Shared
+{
+    // unary
+    public interface IEchoService : IService<IEchoService>
+    {
+        UnaryResult<Nil> Echo(string message);
+    }
+
+    // streaming hub
+    public interface IEchoHubReceiver
+    {
+        void OnSend(MessageResponse message);
+    }
+    public interface IEchoHub : IStreamingHub<IEchoHub, IEchoHubReceiver>
+    {
+        Task<MessageResponse> EchoAsync(string message);
+    }
+
+    [MessagePackObject]
+    public struct MessageResponse
+    {
+        [Key(0)]
+        public string Message { get; set; }
+    }
+}
---
+++ sandbox/EchoMagicOnion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using EchoMagicOnion.Shared;
+using MagicOnion;
+using MagicOnion.Hosting;
+using MagicOnion.Server;
+using MagicOnion.Server.Hubs;
+using MessagePack;
+using Microsoft.Extensions.Hosting;
+
+namespace EchoMagicOnion
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+
+            await MagicOnionHost.CreateDefaultBuilder()
+                .UseMagicOnion()
+                .RunConsoleAsync();
+        }
+    }
+
+    public class EchoService : ServiceBase<IEchoService>, IEchoService
+    {
+        public UnaryResult<Nil> Echo(string message)
+        {
+            return UnaryResult(Nil.Default);
+        }
+    }
+
+    public class EchoHub : StreamingHubBase<IEchoHub, IEchoHubReceiver>, IEchoHub
+    {
+        public Task<MessageResponse> EchoAsync(string message)
+        {
+            var response = new MessageResponse { Message = message };
+            return Task.FromResult(response);
+        }
+    }
+}
