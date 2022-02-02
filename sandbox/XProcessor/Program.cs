3d0cfa26709cebc145bcfe501ce5cc621fd21d52
neuecc <ils@neue.cc>
2022-02-02 18:39:20 +0900

    XP

---
+++ sandbox/XProcessor/Program.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Diagnostics;
+using Zx;
+
+const int ProcessCount = 100;
+
+ConsoleApp.Run(args, async (ConsoleAppContext ctx) =>
+{
+    await $"cd ../../../../ConsoleApp2/bin/Debug/net6.0/";
+
+    Console.WriteLine("Run ConsoleApp2, Count:" + ProcessCount);
+    var tasks = new List<Task>();
+
+    for (int i = 0; i < ProcessCount; i++)
+    {
+        var t = ProcessX.StartAsync("ConsoleApp2.exe").FirstOrDefaultAsync(ctx.CancellationToken);
+        tasks.Add(t);
+    }
+
+    await Task.WhenAll(tasks);
+});
