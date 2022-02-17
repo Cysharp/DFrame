a9f824145b1bbb8205050c40ec332e7405e923cc
neuecc <ils@neue.cc>
2022-02-17 19:37:57 +0900

    component ka!

---
+++ src/DFrame.Controller/Pages/ExecutionResultTable.razor.cs
@@ -0,0 +1,16 @@
+﻿using DFrame.Controller;
+using Microsoft.AspNetCore.Components;
+
+namespace DFrame.Pages;
+
+public partial class ExecutionResultTable
+{
+    [Parameter, EditorRequired]
+    public bool IsRunning { get; set; }
+
+    [Parameter, EditorRequired]
+    public ExecutionSummary? ExecutionSummary { get; set; }
+
+    [Parameter, EditorRequired]
+    public SummarizedExecutionResult[] ExecutionResults { get; set; } = default!;
+}
