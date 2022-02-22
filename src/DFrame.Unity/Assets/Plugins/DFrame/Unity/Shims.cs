d8d92790e673a1d0230c477019b5e847ffc2d720
neuecc <ils@neue.cc>
2022-02-22 10:23:35 +0900

    unity proj

---
+++ src/DFrame.Unity/Assets/Plugins/DFrame/Unity/Shims.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFrame
+{
+    internal static class Shims
+    {
+        internal static bool TryAdd(this Dictionary<string, DFrameWorkloadTypeInfo> dict, string key, DFrameWorkloadTypeInfo value)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, value);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    internal delegate object ObjectFactory(IServiceProvider serviceProvider, object[] arguments);
+
+    internal static class ActivatorUtilities
+    {
+        internal static ObjectFactory CreateFactory(Type type, Type[] _)
+        {
+            return (__, args) => Activator.CreateInstance(type, args);
+        }
+    }
+}
