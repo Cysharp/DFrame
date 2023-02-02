f629c927e96dacef2bfa432c74d559ef993b5c7d
tetsuzin <t.tetsuzin@gmail.com>
2023-02-02 14:37:09 +0900

    add Concurrency and TotalRequestsCount to WorkloadContext

--- src/DFrame.Unity/Assets/Plugins/DFrame/Runtime/WorkloadContext.cs
+++ src/DFrame.Unity/Assets/Plugins/DFrame/Runtime/WorkloadContext.cs
@@ -8,14 +8,18 @@ namespace DFrame
         public WorkloadId WorkloadId { get; }
         public int WorkloadCount { get; }
         public int WorkloadIndex { get; }
+        public int Concurrency { get; }
+        public long TotalRequestCount { get; }
         public CancellationToken CancellationToken { get; }
 
-        public WorkloadContext(ExecutionId executionId, int count, int index, CancellationToken cancellationToken)
+        public WorkloadContext(ExecutionId executionId, int count, int index, int concurrency, long totalRequestCount, CancellationToken cancellationToken)
         {
             this.ExecutionId = executionId;
             this.WorkloadId = WorkloadId.New();
             this.WorkloadCount = count;
             this.WorkloadIndex = index;
+            this.Concurrency = concurrency;
+            this.TotalRequestCount = totalRequestCount;
             this.CancellationToken = cancellationToken;
         }
     }
