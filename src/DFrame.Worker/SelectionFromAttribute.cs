350e71af0d3971971a533a50186e412df4c0b76a
Shoichi Yasui <syasui@orange-cube.jp>
2023-02-18 16:15:14 +0900

    feat: Add SelectionFromAttribute

---
+++ src/DFrame.Worker/SelectionFromAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DFrame;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public class SelectionFromAttribute : Attribute
+{
+    /// <summary>
+    /// Name of Selector method
+    /// </summary>
+    /// <remarks>
+    /// Example for:
+    /// <![CDATA[
+    /// private static IEnumerable<(string label, int value)> GetSomeParameterSelection() => ...;
+    /// public SomeWorkload([SelectionFrom(nameof(GetSomeParameterSelection))] int someParameter) ...
+    /// ]]>
+    /// </remarks>
+    public string SelectorMethodName { get; }
+
+    public SelectionFromAttribute(string selectorMethodName) => SelectorMethodName = selectorMethodName;
+}
