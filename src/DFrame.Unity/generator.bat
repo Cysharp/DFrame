6edb225f741531582b1ff3f85401275e6bb173ed
Mayuki Sawatari <mayuki+github@misuzilla.org>
2024-07-30 12:26:23 +0900

    Update generator.bat

--- src/DFrame.Unity/generator.bat
+++ src/DFrame.Unity/generator.bat
@@ -1,5 +1,2 @@
 dotnet tool restore
 dotnet mpc -i .\Assets\Plugins\DFrame\Runtime\Unity -o .\Assets\Plugins\DFrame\Runtime\Unity\MessagePackGenerated.cs -n DFrame
-
-dotnet build .\DFrame.HubDefinition.csproj
-dotnet moc -i .\DFrame.HubDefinition.csproj -o .\Assets\Plugins\DFrame\Runtime\Unity\MagicOnionGenerated.cs -n DFrame -m DFrame
